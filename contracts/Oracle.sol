dc471fee52845671dacfad0c0f433b79ae2eeb67
chaoxinhu <chaoxinhu@hotmail.com>
2019-09-10 20:23:54 +0800

    exchangerate on chain

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -1,22 +1,21 @@
 pragma solidity ^0.4.25;
 
-import "./Datastore.sol";
+import "./OrgDatastore.sol";
 
 contract Oracle {
-    // The static oracle example
-
-    mapping (bytes32 => address) private datastoreAddressMap;
+    // key: org name, value: org datastore address
+    mapping (bytes32 => address) private addrMap;
 
     function putDatastore(bytes32 id, address addr) public {
-        datastoreAddressMap[id] = addr;
+        addrMap[id] = addr;
     }
 
     function getDatastore(bytes32 id) public view returns (address) {
-        return datastoreAddressMap[id];
+        return addrMap[id];
     }
 
-    function query(bytes32 id, bytes32 request) public view returns (uint256) {
-        Datastore datastore = Datastore(datastoreAddressMap[id]);
+    function query(bytes32 id, bytes32 request) public view returns (bytes32) {
+        OrgDatastore datastore = OrgDatastore(addrMap[id]);
         return datastore.get(request);
     }
 }
